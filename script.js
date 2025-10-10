2ce43041edf8146282807ec350448093e4ec5d43
ZeroXprime <wesleyh-glastonbury@thecoderschool.com>
2025-10-10 15:57:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+function playAudio(){
 var sounds = [
-"999-social-credit-siren.mp3",
-  "applepay.mp3"
-
-  ]
-
-var a = new Audio(sounds[0])
-a.play()
+  "999-social-credit-siren.mp3",
+    "applepay.mp3"
+  
+    ]
+  
+  var a = new Audio(sounds[0])
+  a.play()
+}
