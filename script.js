de5f5093f32d0ea19a5ac3c53148666f04b0cfc3
ZeroXprime <wesleyh-glastonbury@thecoderschool.com>
2026-01-07 16:32:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ var sounds = [
   var a = new Audio(sounds[r])
   a.play()
 }
+var c = document.getElementById("bob")
+
+var hight_limeted = 250
+var hight = 200
+var gravity = 5
+var yspeed = 67
+var jumppower = 67
+
+function jump() {
+  yspeed -= gravity;
+  if (hight <= 0){
+    yspeed = 0
+  }
+  hight += yspeed;
+  
+  
+  c.style.marginTop = hight.toString();
+}
+
+function boost() {
+  hight = 10;
+  yspeed = 100;
+}
+
+
+
+setInterval(jump, 33)
+document.addEventListener('keydown', function(event){
+  yspeed = 100;
+})
